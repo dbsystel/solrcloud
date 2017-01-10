276b5a158f29203fe81e7f9d11ae521c07f873bd
Jakob Frank <jakob@apache.org>
2017-01-10 13:57:29 +0100

    - Allow listing configSets - Allow creation based on existing configSets

--- solrcloud-management/src/main/java/db/garagedays/solrcloud/webservice/AdminWebservice.java
+++ solrcloud-management/src/main/java/db/garagedays/solrcloud/webservice/AdminWebservice.java
@@ -1,18 +1,28 @@
 package db.garagedays.solrcloud.webservice;
 
 import db.garagedays.solrcloud.exception.InstanceInitException;
-import db.garagedays.solrcloud.service.ApplicationService;
+import db.garagedays.solrcloud.model.SolrInstance;
+import db.garagedays.solrcloud.service.SolrCloudService;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * @author Thomas Kurz (tkurz@apache.org)
@@ -21,29 +31,72 @@ import java.io.IOException;
 @RestController
 public class AdminWebservice {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
-    ApplicationService service;
+    SolrCloudService service;
 
     @PostMapping("/create")
-    public String handleFileUpload(
-            @RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<?> createCollection(
+            @RequestParam(value = "name", required = false, defaultValue = "collection1") String collectionName,
+            @RequestParam(value = "configName", required = false) String configName,
+            @RequestParam(value = "file", required = false) MultipartFile configZip
+    ) throws IOException {
 
-        try {
-            service.create(convert(file));
-        } catch (InstanceInitException e) {
-            e.printStackTrace();
+        if (configName == null && configZip == null) {
+            return ResponseEntity.badRequest().body("One of 'configName' or 'file' is required");
+        } else if (configZip != null) {
+            final Path confDir = unpackUpload(configZip);
+            try {
+                final SolrInstance instance = service.createCollection(collectionName, confDir);
+            return ResponseEntity.created(getInstanceLocation(instance)).body(instance);
+            } catch (InstanceInitException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+            } finally {
+                Files.deleteIfExists(confDir);
+            }
+        } else {
+            try {
+                final SolrInstance instance = service.createCollection(collectionName, configName);
+                return ResponseEntity.created(getInstanceLocation(instance)).body(instance);
+            } catch (InstanceInitException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+            }
         }
-
-        return "You successfully uploaded " + file.getOriginalFilename() + "!";
     }
 
-    public File convert(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        convFile.createNewFile();
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
+    private URI getInstanceLocation(SolrInstance instance) {
+        // FIXME: maybe this causes a NullPointerException!
+        return null;
+    }
+
+    @GetMapping("/configurations")
+    public List<String> listConfigurations() {
+        return service.listConfigSets();
+    }
+
+    private Path unpackUpload(MultipartFile configZip) throws IOException {
+        final Path tempDir = Files.createTempDirectory(StringUtils.defaultString(configZip.getOriginalFilename(), "collection-config"));
+        try {
+            try (ZipInputStream zis = new ZipInputStream(configZip.getInputStream())) {
+                for (ZipEntry e = zis.getNextEntry(); e != null; e = zis.getNextEntry()) {
+                    final String name = e.getName();
+                    if (e.isDirectory()) {
+                        Files.createDirectories(tempDir.resolve(name));
+                    } else {
+                        Files.copy(zis, tempDir.resolve(name));
+                    }
+                }
+            }
+            log.info("File unziped to temporary directory {}", tempDir);
+
+        } catch (final Throwable t) {
+            FileUtils.deleteDirectory(tempDir.toFile());
+            throw t;
+        }
+        return tempDir;
     }
 
 }
